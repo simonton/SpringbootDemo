0ab09eb066b320986dd4f7a276029b982f4f2b64
simonton <xia.simonton@gmail.com>
2019-04-09 22:28:23 +0800

    Integrate mybatis framwork

---
+++ MybatisDemo/src/test/java/com/simonton/mybatis/mybatisdemo/MybatisdemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.simonton.mybatis.mybatisdemo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+@SpringBootTest(classes = {MybatisdemoApplication.class})
+public class MybatisdemoApplicationTests extends AbstractTestNGSpringContextTests{
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
