504e39dfd650cdc9dbcead581d5c2654a6dabb29
simonton <xia.simonton@gmail.com>
2019-04-10 23:09:18 +0800

    add mybatis code generator

--- MybatisDemo/src/test/java/com/simonton/mybatis/mybatisdemo/MybatisdemoApplicationTests.java
+++ MybatisDemo/src/test/java/com/simonton/mybatis/mybatisdemo/MybatisdemoApplicationTests.java
@@ -1,14 +1,32 @@
 package com.simonton.mybatis.mybatisdemo;
 
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest(classes = {MybatisdemoApplication.class})
 public class MybatisdemoApplicationTests extends AbstractTestNGSpringContextTests{
 
 	@Test
-	public void contextLoads() {
+	public void testGenerator() throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		//指向逆向工程配置文件
+		File configFile = new File("generatorConfig.xml");
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
 	}
 
 }
